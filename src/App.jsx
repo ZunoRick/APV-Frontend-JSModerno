b2eaf9885f9b84de4a905533cd7a31d3c0316eaa
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2022-03-23 23:34:33 -0600

    Creando Context y autenticando usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,25 @@ import OlvidePassword from './paginas/OlvidePassword';
 import ConfirmarCuenta from './paginas/ConfirmarCuenta';
 import NuevoPassword from './paginas/NuevoPassword';
 
+import { AuthProvider } from './context/AuthProvider';
+
 function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<AuthLayout />}>
-          <Route index element={<Login />} />
-          <Route path='registrar' element={<Registrar />} />
-          <Route path='olvide-password' element={<OlvidePassword />} />
-          <Route path='olvide-password/:token' element={<NuevoPassword />} />
-          <Route path='confirmar/:token' element={<ConfirmarCuenta />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path='/' element={<AuthLayout />}>
+            <Route index element={<Login />} />
+            <Route path='registrar' element={<Registrar />} />
+            <Route path='olvide-password' element={<OlvidePassword />} />
+            <Route path='olvide-password/:token' element={<NuevoPassword />} />
+            <Route path='confirmar/:token' element={<ConfirmarCuenta />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
 
-export default App
+export default App;
--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -1,6 +1,39 @@
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import clienteAxios from '../config/axios';
+import Alerta from '../components/Alerta';
+import useAuth from '../hooks/useAuth';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+	const [alerta, setAlerta] = useState({});
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+
+		if ([email, password].includes('')) {
+			setAlerta({ msg: 'Todos los campos son obligatorios', error: true });
+			return;
+		}
+
+    try {
+      const { data } = await clienteAxios.post('/veterinarios/login', { email, password});
+      localStorage.setItem('apv_token', data.token);
+      navigate('/admin');
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true
+      });
+    }
+  }
+
+  const { msg } = alerta;
+
   return (
     <>
         <div>
@@ -11,7 +44,8 @@ const Login = () => {
         </div>
 
         <div className='mt-20 md:mt-5 shadow-lg px-8 py-10 rounded-xl bg-white'>
-          <form action="">
+				{msg && <Alerta alerta={alerta} />}
+          <form onSubmit={handleSubmit}>
             <div className="my-5">
               <label 
                 className="uppercase text-gray-600 block text-xl font-bold"
@@ -25,6 +59,8 @@ const Login = () => {
                 type="email"
                 placeholder="Email de Registro"
                 className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
               />
             </div>
 
@@ -41,6 +77,8 @@ const Login = () => {
                 type="password"
                 placeholder="Tu Password"
                 className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
               />
             </div>
 
